60c8c491c4e8d141335ea460fbc5c39819ca849e
responsiblehemmher <hemmher12@gmail.com>
2024-04-22 15:23:07 +0100

    res-hemmherfirst commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import Repositories from './components/repositoriies/Repositories';
+
+function App() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <Repositories />
+    </div>
+  );
+}
+
+export default App;
